55e21a1f61f668b930e54762d3f7467997f3bb69
Armando <flores@mtu.edu>
2016-04-04 21:51:50 -0400

    Updates as per requests

---
+++ js/ziero.js
@@ -0,0 +1,37 @@
+
+// Allows for page easing
+$('.page-scroll').bind('click', function(event) {
+  var $anchor = $(this);
+  $('html, body').stop().animate({
+    scrollTop: $($anchor.attr('href')).offset().top
+  }, 1800, 'easeInOutExpo');
+  event.preventDefault();
+});
+
+
+
+// Waypoints
+$('#wellness-trigger').waypoint( function(){
+  document.getElementById("wellness-copy").classList.add("active");
+  document.getElementById("wellness-row").classList.add("active");
+});
+
+$('#employee-trigger').waypoint( function(){
+  document.getElementById("employee-row").classList.add("active");
+});
+
+
+$('#pricing').waypoint( function(){
+  document.getElementById("nonprofit-modal").classList.add("active");
+});
+
+
+$('#nonprofit-trigger').waypoint( function(){
+  document.getElementById("nonprofit-modal").classList.add("active");
+});
+
+
+
+
+
+// document.getElementById("published-sidebar").insertRule("display {inactive")
